b05bb71f6a36ab8a6133e2633b2e1e512cf864f8
Pritul Gohil <34166907+pritulgohil@users.noreply.github.com>
2025-03-14 16:26:05 -0400

    pos sidebar started

--- src/app/components/Pos/Sidebar/Sidebar.jsx
+++ src/app/components/Pos/Sidebar/Sidebar.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import styles from "./Sidebar.module.css";
+import { LayoutDashboard } from "lucide-react";
+import { ArrowRightLeft } from "lucide-react";
+import { TableCellsMerge } from "lucide-react";
+import { Soup } from "lucide-react";
+import { Users } from "lucide-react";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <>
+      <div className={`${styles.mainContainer} shadow-md`}>
+        <div className={styles.topSidebar}>
+          <ul>
+            <li>
+              <LayoutDashboard className={styles.sidebarIcon} />
+              Dashboard
+            </li>
+            <li>
+              <ArrowRightLeft className={styles.sidebarIcon} />
+              Order Line
+            </li>
+            <li>
+              <TableCellsMerge className={styles.sidebarIcon} />
+              Manage Table
+            </li>
+            <li>
+              <Soup className={styles.sidebarIcon} />
+              Manage Dishes
+            </li>
+            <li>
+              <Users className={styles.sidebarIcon} />
+              Customers
+            </li>
+          </ul>
+        </div>
+        <div className={styles.bottomSidebar}></div>
+      </div>
+    </>
+  );
 };
 
 export default Sidebar;
--- src/app/pos/page.jsx
+++ src/app/pos/page.jsx
@@ -1,8 +1,11 @@
 import Navbar from "../components/Pos/Navbar/Navbar";
+import Sidebar from "../components/Pos/Sidebar/Sidebar";
+
 const page = () => {
   return (
     <>
       <Navbar />
+      <Sidebar />
     </>
   );
 };
