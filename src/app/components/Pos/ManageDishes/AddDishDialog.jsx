c38475670d6340c9e19669cff29bfc68957420c4
Pritul Gohil <34166907+pritulgohil@users.noreply.github.com>
2025-04-17 22:34:41 -0400

    fixed card width issues

--- src/app/components/Pos/ManageDishes/AddDishDialog.jsx
+++ src/app/components/Pos/ManageDishes/AddDishDialog.jsx
@@ -11,6 +11,13 @@ import {
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectTrigger,
+  SelectContent,
+  SelectItem,
+  SelectValue,
+} from "@/components/ui/select";
 
 const AddDishDialog = ({ children }) => {
   return (
@@ -24,17 +31,33 @@ const AddDishDialog = ({ children }) => {
           </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
-          <div>
+          <div className="flex flex-col gap-2">
             <label className="block text-sm font-medium">Dish Name</label>
             <Input placeholder="e.g., Belgian Waffle" />
           </div>
-          <div>
-            <label className="block text-sm font-medium">Category</label>
+          <div className="flex flex-col gap-2">
+            <label className="block text-sm font-medium">Description</label>
             <Input placeholder="e.g., Dessert" />
           </div>
-          <div>
+          <div className="flex flex-col gap-2">
+            <label className="block text-sm font-medium">Emoji</label>
+            <Input placeholder="e.g., Emoji" />
+          </div>
+          <div className="flex flex-col gap-2">
             <label className="block text-sm font-medium">Price</label>
             <Input placeholder="$8.00" />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label className="block text-sm font-medium">Available</label>
+            <Select>
+              <SelectTrigger>
+                <SelectValue placeholder="Select availability" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="true">True</SelectItem>
+                <SelectItem value="false">False</SelectItem>
+              </SelectContent>
+            </Select>
           </div>
         </div>
         <DialogFooter>
