4c472309ea4a80a9a246213b05f804122bbe1824
Pritul Gohil <34166907+pritulgohil@users.noreply.github.com>
2025-08-28 22:44:10 -0400

    header for order board complete

--- src/app/pos/order-board/page.jsx
+++ src/app/pos/order-board/page.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import styles from "./page.module.css";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.header}>
+        <h2>Order Board</h2>
+      </div>
+    </div>
+  );
 };
 
 export default page;
