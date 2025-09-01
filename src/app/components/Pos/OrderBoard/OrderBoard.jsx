0a79c566122003ee32fdfd3eaf141217c3250457
Pritul Gohil <34166907+pritulgohil@users.noreply.github.com>
2025-09-01 10:15:13 -0400

    order board card frontend complete

--- src/app/components/Pos/OrderBoard/OrderBoard.jsx
+++ src/app/components/Pos/OrderBoard/OrderBoard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./OrderBoard.module.css";
-import { Hourglass, User, Clock } from "lucide-react";
+import { Hourglass, User, Clock, Rocket } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export const OrderBoard = () => {
   return (
@@ -40,7 +41,7 @@ export const OrderBoard = () => {
         </div>
       </div>
       <div className={styles.orderCardContainer}>
-        <div className={styles.orderCard}>
+        <div className={`${styles.orderCard} shadow-md`}>
           <div className={styles.firstContainer}>
             <div className={styles.orderId}>Order #3dbefr</div>
             <div className={styles.orderStatus}>
@@ -58,6 +59,26 @@ export const OrderBoard = () => {
               <Clock size={12} />
               Just Now
             </div>
+          </div>
+          <div className={styles.thirdContainer}>
+            <div className={styles.orderItemContainer}>
+              <div className={styles.itemName}>Caesar Salad</div>
+              <div className={styles.itemCount}>x2</div>
+            </div>
+            <div className={styles.orderItemContainer}>
+              <div className={styles.itemName}>Grilled Salmon</div>
+              <div className={styles.itemCount}>x1</div>
+            </div>
+            <div className={styles.orderItemContainer}>
+              <div className={styles.itemName}>Sparkling Water</div>
+              <div className={styles.itemCount}>x1</div>
+            </div>
+          </div>
+          <div className={styles.buttonContainer}>
+            <Button className={styles.startPrepButton}>
+              <Rocket />
+              Start Preparing
+            </Button>
           </div>
         </div>
       </div>
