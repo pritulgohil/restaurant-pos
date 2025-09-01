795573f1a3522382e95c4b39fe60705a5461e916
Pritul Gohil <34166907+pritulgohil@users.noreply.github.com>
2025-08-31 22:56:45 -0400

    change in capsule design

--- src/app/components/Pos/OrderBoard/OrderBoard.jsx
+++ src/app/components/Pos/OrderBoard/OrderBoard.jsx
@@ -1,28 +1,44 @@
 import React from "react";
 import styles from "./OrderBoard.module.css";
-import { Hourglass, CircleCheckBig } from "lucide-react";
 
 export const OrderBoard = () => {
   return (
     <div className={styles.mainContainer}>
       <div className={styles.cardCapsuleContainer}>
+        <div className={`${styles.cardCapsule} ${styles.allCapsule}`}>
+          <div className="capsuleHeader">All</div>
+          <div
+            className={`${styles.capsuleIconContainer} ${styles.allIconContainer}`}
+          >
+            07
+          </div>
+        </div>
+        <div className={`${styles.cardCapsule} ${styles.inProgressCapsule}`}>
+          <div className="capsuleHeader">In Progress</div>
+          <div
+            className={`${styles.capsuleIconContainer} ${styles.inProgressIconContainer}`}
+          >
+            07
+          </div>
+        </div>
         <div className={`${styles.cardCapsule} ${styles.pendingCapsule}`}>
+          <div className="capsuleHeader">Pending</div>
           <div
             className={`${styles.capsuleIconContainer} ${styles.pendingIconContainer}`}
           >
-            <Hourglass className={styles.capsuleIcon} />
+            07
           </div>
-          <div className="capsuleHeader">Pending</div>
         </div>
         <div className={`${styles.cardCapsule} ${styles.completeCapsule}`}>
+          <div className="capsuleHeader">Completed</div>
           <div
             className={`${styles.capsuleIconContainer} ${styles.completeIconContainer}`}
           >
-            <CircleCheckBig className={styles.completeCapsuleIcon} />
+            12
           </div>
-          <div className="capsuleHeader">Completed</div>
         </div>
       </div>
+      <div className={styles.orderCardContainer}></div>
     </div>
   );
 };
