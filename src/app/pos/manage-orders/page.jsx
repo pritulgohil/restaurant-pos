cfc0fcca22e6ef5a98d7408c16bb9d4fedef3d35
Pritul Gohil <34166907+pritulgohil@users.noreply.github.com>
2025-09-19 23:51:08 -0400

    initiated orderboard component and styles

--- src/app/pos/manage-orders/page.jsx
+++ src/app/pos/manage-orders/page.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import styles from "./page.module.css";
+import ManageOrders from "@/app/components/Pos/ManageOrders/ManageOrders";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.topContainer}>
+        <div className={styles.header}>
+          <h2>Manage Orders</h2>
+        </div>
+      </div>
+      <div className={styles.bottomContainer}>
+        <ManageOrders />
+      </div>
+    </div>
+  );
 };
 
 export default page;
