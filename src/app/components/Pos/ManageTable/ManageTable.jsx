bd811fa3123bbc9b6a5dc1972821f48b4be6503d
Pritul Gohil <34166907+pritulgohil@users.noreply.github.com>
2025-11-08 23:08:37 -0500

    added files and layout for manage table section

--- src/app/components/Pos/ManageTable/ManageTable.jsx
+++ src/app/components/Pos/ManageTable/ManageTable.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./ManageTable.module.css";
 import { Input } from "@/components/ui/input";
-import { Plus } from "lucide-react";
+import { Plus, Armchair, CircleUser, Phone, Check } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 const ManageTable = () => {
@@ -26,15 +26,39 @@ const ManageTable = () => {
           <div className={styles.customerContainer}>
             <div className={styles.customerCardContainer}>
               <div className={styles.customerCard}>
-                <div className={styles.timeCard}>7:30 PM</div>
+                <div className={styles.timeCard}>
+                  7:30
+                  <br />
+                  PM
+                </div>
+                <div className={styles.customerDetails}>
+                  <div className={styles.customerName}>John Doe</div>
+                  <div className={styles.tableDetails}>
+                    <div className={styles.table}>
+                      <Armchair size={16} color="gray" />
+                      02
+                    </div>
+                    <div className={styles.people}>
+                      <CircleUser size={16} color="gray" />4
+                    </div>
+                  </div>
+                  <div className={styles.phoneNumber}>
+                    <Phone size={16} color="gray" />
+                    (519)-740-9945
+                  </div>
+                </div>
+                <div className={styles.paymentStatus}>
+                  <div className={styles.checkIconContainer}>
+                    <Check size={10} color="white" strokeWidth={3} />
+                  </div>
+                  Payment
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </>
-
-    // There will be some connection between manage table and order line for customers
   );
 };
 
