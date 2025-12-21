055a81eb18a9bb48cd1c194651236ba5690084ed
Pritul Gohil <34166907+pritulgohil@users.noreply.github.com>
2025-12-21 12:19:42 -0500

    added coming soon page

---
+++ src/app/coming-soon/page.jsx
@@ -0,0 +1,43 @@
+"use client";
+import React from "react";
+import styles from "./page.module.css";
+import { Button } from "@/components/ui/button";
+import { ChevronLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+const page = () => {
+  const router = useRouter();
+  return (
+    <div className={styles.pageWrapper}>
+      <video className={styles.backgroundVideo} autoPlay muted loop playsInline>
+        <source src="/background.mp4" type="video/mp4" />
+      </video>
+
+      <div className={styles.overlay} />
+
+      <div className={styles.mainContainer}>
+        <div className={styles.contentContainer}>
+          <div className={styles.siteHeader}>Plates Up</div>
+          <div className={styles.comingSoon}>Coming Soon!</div>
+          <div className={styles.description}>
+            We’re building something great here. <br />
+            This page will be available in a future update.
+          </div>
+          <div className={styles.backButton}>
+            <Button
+              variant="ghost"
+              size="lg"
+              className="border border-2 flex items-center gap-2"
+              onClick={() => router.push("/dashboard")}
+            >
+              <ChevronLeft strokeWidth={3} />
+              Back to Dashboard
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
