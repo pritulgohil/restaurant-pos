662f4ed4c442cddeb0176eca1c1cc772fa04b1d2
Pritul Gohil <34166907+pritulgohil@users.noreply.github.com>
2025-12-21 15:44:09 -0500

    switched to mongodb atlas

--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -1,8 +1,18 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = "mongodb://127.0.0.1:27017/platesup";
+const MONGODB_URI = process.env.MONGODB_URI;
 
-let cached = global.mongoose || { conn: null, promise: null };
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local or Vercel settings"
+  );
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 async function dbConnect() {
   if (cached.conn) return cached.conn;
