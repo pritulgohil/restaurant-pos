c65c77b76fa37816971e34ace05ca39fde0eeaa5
Pritul Gohil <34166907+pritulgohil@users.noreply.github.com>
2026-01-11 10:44:34 -0500

    updated delete notification api to use cookies for token

--- src/app/api/notification/delete-notification/[notificationId]/route.js
+++ src/app/api/notification/delete-notification/[notificationId]/route.js
@@ -2,24 +2,16 @@ import { NextResponse } from "next/server";
 import dbConnect from "@/lib/dbConnect";
 import Notification from "@/models/notification";
 import mongoose from "mongoose";
-import jwt from "jsonwebtoken";
-
-const JWT_SECRET = process.env.JWT_SECRET;
+import { verifyAuth } from "@/lib/verifyAuth";
 
 export async function DELETE(req, { params }) {
   try {
     await dbConnect();
 
-    const authHeader = req.headers.get("authorization");
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-    }
-
-    const token = authHeader.split(" ")[1];
     let decoded;
 
     try {
-      decoded = jwt.verify(token, JWT_SECRET);
+      decoded = verifyAuth(req);
     } catch (err) {
       return NextResponse.json(
         { error: "Invalid or expired token" },
