9adf407f5e3695d3e5f658f8785f23b449cc6664
Pritul Gohil <34166907+pritulgohil@users.noreply.github.com>
2026-01-11 14:09:19 -0500

    implemented cookies on fetch-all-orders api

--- src/app/api/pos/fetch-all-orders/[restaurantId]/route.js
+++ src/app/api/pos/fetch-all-orders/[restaurantId]/route.js
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import dbConnect from "@/lib/dbConnect";
 import Order from "@/models/order";
 import jwt from "jsonwebtoken";
+import { verifyAuth } from "@/lib/verifyAuth";
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
@@ -9,15 +10,9 @@ export async function GET(req, { params }) {
   try {
     await dbConnect();
 
-    const authHeader = req.headers.get("authorization");
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-    }
-
-    const token = authHeader.split(" ")[1];
     let decoded;
     try {
-      decoded = jwt.verify(token, JWT_SECRET);
+      decoded = verifyAuth(req);
     } catch (err) {
       return NextResponse.json(
         { error: "Invalid or expired token" },
