a4182fe1710a8ec123e80dd07814fe47ab298113
Pritul Gohil <34166907+pritulgohil@users.noreply.github.com>
2026-01-11 14:00:56 -0500

    implement cookies in edit-category api

--- src/app/api/pos/edit-category/[categoryId]/route.js
+++ src/app/api/pos/edit-category/[categoryId]/route.js
@@ -2,25 +2,16 @@ import { NextResponse } from "next/server";
 import dbConnect from "@/lib/dbConnect";
 import Category from "@/models/category";
 import Dish from "@/models/dish";
-import jwt from "jsonwebtoken";
-
-const JWT_SECRET = process.env.JWT_SECRET;
+import { verifyAuth } from "@/lib/verifyAuth";
 
 export async function PATCH(req, { params }) {
   try {
     await dbConnect();
 
-    // Auth
-    const authHeader = req.headers.get("authorization");
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-    }
-
-    const token = authHeader.split(" ")[1];
     let decoded;
 
     try {
-      decoded = jwt.verify(token, JWT_SECRET);
+      decoded = verifyAuth(req);
     } catch (err) {
       return NextResponse.json(
         { error: "Invalid or expired token" },
